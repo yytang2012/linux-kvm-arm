e088f52dc9abcc75737de0d8252fa6de8d27bc98
yytang <yytang@cs.wm.edu>
2014-08-06 10:07:32 -0400

    This branch is for exynos5250 development board. We get the source code from https://github.com/ITRI-CCMA-hypervisor/linux-kvm-arm

---
+++ make_yytang.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export ARCH=arm 
+export CROSS_COMPILE=arm-linux-gnueabi-
+echo "compilng dtbs ..."
+make dtbs
+echo "copy exynos5250-arndale.dtb to images-to-fuse "
+cp arch/arm/boot/dts/exynos5250-arndale.dtb ~/virtualization/images-to-fuse/
+
+echo "compilng uImage..."
+make uImage -j8
+echo "copy uImage to images-to-fuse"
+cp arch/arm/boot/uImage ~/virtualization/images-to-fuse/
+
+echo "compilng modules..."
+make modules
+
+mkdir -p modules
+make modules_install INSTALL_MOD_PATH=modules
+mv modules ~/virtualization/images-to-fuse/modules
+echo "done"
+
